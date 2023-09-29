09fbac4f416d841cc29d07f9a783ee10de9240c4
zhenhuaizeng <zengzhenhuai8@gmail.com>
2023-09-29 10:48:28 -0700

    Reverted to a previous version of my Point class. Committing changes to alter the (x, y) coordinate to a (x, y, z ) coordinate.

--- SDEV372_lab1/src/main/java/org/example/Point.java
+++ SDEV372_lab1/src/main/java/org/example/Point.java
@@ -4,11 +4,31 @@ public class Point {
     private double x;
     private double y;
 
-    public Point(double x, double y)
-    {
+    private double z;
+
+    public Point(double x, double y, double z) {
         this.x = x;
         this.y = y;
+        this.z = z;
+    }
+    public double getX() {
+        return x;
     }
 
+    public double getY() {
+        return y;
+    }
 
+    public double getZ() {
+        return z;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
 }
