cc2b50145f350361ca0fbe517b2a5b1ca075f7d3
zhenhuaizeng <zengzhenhuai8@gmail.com>
2023-09-29 10:34:11 -0700

    Added an equals() and hashcode() method.

--- SDEV372_lab1/src/main/java/org/example/Point.java
+++ SDEV372_lab1/src/main/java/org/example/Point.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Point {
     private double x;
     private double y;
@@ -8,6 +10,44 @@ public class Point {
     {
         this.x = x;
         this.y = y;
+    }
+
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(x, point.x) == 0 && Double.compare(y, point.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 
 
